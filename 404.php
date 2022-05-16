b9f74cb06ec90c8c65ed787dcdb5964668ae421d
Lautaro Settembrini <lautarosettembrini@hotmail.com>
2022-05-15 22:50:19 -0300

    error404

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@800&family=Marcellus&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="./Img/favicon.png">
+    <title>ORBIS TERRARUM | Error 404</title>
+</head>
+<body>
+    <h1 class="error404">Error 404</h1>
+    <h2 class="advertencia">Te has caído del mapa...</h2>
+    <h3 class="volver">No te preocupes, puedes volver haciendo clic en la rueda.</h3>
+    <div class="rueda">
+        <a href="./index.html"><img class="logo" src="./img/logogiratorio.png" alt="logo"></a>
+    </div>
+</body>
+</html>
